6ca6944b456b6af42d32536dedae3b90dbd647ad
GDMane <2611ganesh@gmail.com>
2019-03-09 23:36:22 +0530

    GM Framework demo

---
+++ MyParent.py
@@ -0,0 +1,14 @@
+from MyUtilities import MyUtilitiesClass
+
+class MyParentClass:
+
+    def MyClick(self,MyElement):
+        MyElement.click()
+
+
+    def MySendKeys(self,MyElement,MyValue):
+        MyElement.send_keys(MyValue)
+
+    def MyMouseOver(self,MyElement):
+        pass
+
